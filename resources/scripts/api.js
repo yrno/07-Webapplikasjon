412aafda6a3f25c216d0df05267ab78917e7cee2
yrno <henriksfurdal@outlook.com>
2022-02-03 00:47:05 +0100

    Added "Deck of Cards API"

--- resources/scripts/api.js
+++ resources/scripts/api.js
@@ -9,3 +9,19 @@ async function getCatFact() {
     }
 }
 getCatFact();
+async function drawCard() {
+    const data = await fetch("https://deckofcardsapi.com/api/deck/d2tygv6s4gq3/draw/?count=1");
+    const json = await data.json();
+    drawnCard = json.cards[0].code;
+    console.log(drawnCard)
+    drawnCardImage = json.cards[0].image;
+    cardsRemaining = json.remaining;
+    if (drawnCard) {
+        document.getElementById("card_much").innerHTML = "<img src=\""+drawnCardImage+"\"> <p>"+cardsRemaining+" cards left in the deck</p>";
+    }
+}
+async function shuffleDeck() {
+    const x = await fetch("https://deckofcardsapi.com/api/deck/d2tygv6s4gq3/shuffle/")
+    drawCard();
+}
+shuffleDeck();
